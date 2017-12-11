29268d14b9f9bcec46630cef47d15d4976d9dc6b
Jason Pompon <jasonpompon@gmail.com>
2017-12-11 04:52:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ["sickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(myArr, newElem) {
+    return [newElem, ...myArr]
+}
+
+function destructivelyAddElementToBeginningOfArray(myArr, newElem) {
+    return myArr.unshift(newElem);
+}
